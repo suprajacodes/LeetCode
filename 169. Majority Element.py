399efa2aa7c50412063a1c5b56d8d6d87ed6883a
suprajacodes <supraja.asb@gmail.com>
2024-01-09 13:33:11 +0530

    Optimised code

--- 169. Majority Element.py
+++ 169. Majority Element.py
@@ -1,15 +1,4 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        n = len(nums) / 2
-        d = {}
-        for i in nums:
-            if i in d:
-                d[i] += 1
-            else:
-                d[i] = 1
-        m = (max(d.items(), key=lambda x: x[1]))
-        if int(m[1]) > n:
-            return m[0]
-
-
-
+        nums.sort()
+        return (nums[len(nums)//2])

72418e7233ea61153e6dec75a4be52c7db01c290
suprajacodes <supraja.asb@gmail.com>
2024-01-11 23:30:43 +0530

    Optimised

--- 1470. Shuffle the Array.py
+++ 1470. Shuffle the Array.py
@@ -1,11 +1,7 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        nums1 = nums[:n]
-        # print(nums1)
-        nums2 = nums[n:]
-        # print(nums2)
-        res = []
-        for i in range(n):
-            res.append(nums1[i])
-            res.append(nums2[i])
-        return res
+        l = []
+        for i in range (n):
+            l+= [nums[i]]
+            l+=[nums[i+n]]
+        return l

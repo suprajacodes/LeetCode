6e2adb8a5fb8a49a6068690167404746cb157783
suprajacodes <supraja.asb@gmail.com>
2024-01-11 17:11:20 +0530

    Optimised

--- 867. Transpose Matrix.py
+++ 867. Transpose Matrix.py
@@ -1,3 +1,7 @@
-class Solution:
-    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        return (map(list,zip(*matrix)))
+def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+    rows, cols = len(matrix), len(matrix[0])
+    r = [[0] * rows for _ in range(cols)]
+    for i in range(rows):
+        for j in range(cols):
+            r[j][i] = matrix[i][j]
+    return r

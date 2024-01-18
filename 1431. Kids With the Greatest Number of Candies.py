8420baa6c897be2daaef58e08c52b7e08bb3a86f
suprajacodes <supraja.asb@gmail.com>
2024-01-19 00:56:58 +0530

    O(n) and O(n)

---
+++ 1431. Kids With the Greatest Number of Candies.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        l=[]
+        c=[]
+        max(candies)
+        for i in range (len(candies)):
+            c.append(candies[i]+extraCandies)
+            print(c[i])
+            if c[i] >= max(candies):
+                l.append(True)
+            else:
+                l.append(False)
+        return l

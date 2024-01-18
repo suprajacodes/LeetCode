5f39037d804c1bd3513248a48f8442de4bc9837a
suprajacodes <supraja.asb@gmail.com>
2024-01-19 00:12:46 +0530

    Optimised- O(n)- time and space complexity

--- 605. Can Place Flowers.py
+++ 605. Can Place Flowers.py
@@ -1,18 +1,12 @@
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
-
-        l = len(flowerbed)
-        co = 0
-        c = 1
-        flowerbed.insert(0, 0)
-        flowerbed.append(0)
-        print(flowerbed)
-        while (c <= l):
-            if flowerbed[c - 1] != 1 and flowerbed[c] != 1 and flowerbed[c + 1] != 1:
-                flowerbed[c] = 1
+        flowerbed = [0] + flowerbed + [0]
+        for i in range(1, len(flowerbed) - 1):
+            if flowerbed[i - 1] != 1 and flowerbed[i] != 1 and flowerbed[i + 1] != 1:
+                flowerbed[i] = 1
                 n -= 1
-            c += 1
         return n <= 0
+
 
 
 

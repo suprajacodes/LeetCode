f0c2b250d09e156d332209c4ac8f19078a3b7f07
suprajacodes <supraja.asb@gmail.com>
2024-06-12 17:57:54 -0400

    Medium - 75. Sort Colors, T-O(n), S-O(1)

--- 75. Sort Colors.py
+++ 75. Sort Colors.py
@@ -3,15 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # Initialize a count array for numbers 0, 1, and 2
-        count = [0] * 3
-        # Step 1: Count the occurrences of each number
-        for i in nums:
-            count[i]+=1
-        print(count)
-        # Step 2: Clear the original nums list
-        nums.clear()
-        # Step 3: Reconstruct the nums list based on count_array
-        for i, j in enumerate(count):
-            nums.extend([i] * j)
-
+        count=Counter(nums)
+        index = 0
+        for i in range (0,2+1):
+            for _ in range (count[i]):
+                nums[index]=i
+                index += 1

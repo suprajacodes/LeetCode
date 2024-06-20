af09462c9d4586b994d1a098b93e4bbbf1c86809
suprajacodes <supraja.asb@gmail.com>
2024-06-20 10:15:22 +0530

    Easy

---
+++ 1071. Greatest Common Divisor of Strings.py
@@ -0,0 +1,24 @@
+'''
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+'''
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        else:
+            return str2[:math.gcd((len(str1)), len(str2))]
+
+
+
+

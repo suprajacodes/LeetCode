977ba5afeaae97efa801b26059368be04a64151e
suprajacodes <supraja.asb@gmail.com>
2024-06-20 20:08:04 +0530

    Easy

---
+++ 2108. Find First Palindromic String in the Array.py
@@ -0,0 +1,27 @@
+'''
+Example 1:
+
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada".
+Note that "racecar" is also palindromic, but it is not the first.
+Example 2:
+
+Input: words = ["notapalindrome","racecar"]
+Output: "racecar"
+Explanation: The first and only string that is palindromic is "racecar".
+Example 3:
+
+Input: words = ["def","ghi"]
+Output: ""
+Explanation: There are no palindromic strings, so the empty string is returned.
+'''
+
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for i in words:
+            if i == i[::-1]:
+                return i
+        return ""
+

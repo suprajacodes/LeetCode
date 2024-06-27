31df90edc833ad4abc61c82a63588b535a29ee03
suprajacodes <supraja.asb@gmail.com>
2024-06-27 13:18:08 +0530

    Easy

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,31 @@
+'''
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+'''
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        res = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and d[s[i]] < d[s[i + 1]]:
+                res -= d[s[i]]
+            else:
+                res += d[s[i]]
+        return res
+
